8aa6a0b86b7d8e2a0d8447395ac90112bd5cef59
Timofei <tim@tsnoom-laptop.(none)>
2010-03-13 19:07:54 +0300

    more tests

--- tests.py
+++ tests.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import os
+import glob
 import unittest
 from hampy import Template, Loader
 
-loader = Loader('examples')
+loader = Loader('.')
 
-class TagsAndText(unittest.TestCase):
+class Examples(unittest.TestCase):
 
-    def test_tags(self):
-        t = loader.get_template('tags.html')
-        self.assertEqual(t.render(), open('examples/tags.html.result', 'r').read())
+    def test_examples(self):
+        htmls = glob.glob('examples/*.html')
+        results = [r[:-len('.result')]for r in glob.glob('examples/*.result')]
+        for html in htmls:
+            if html in results:
+                print html
+                t = loader.get_template(html)
+                self.assertEqual(t.render(), open(html + '.result', 'r').read(), html)
 
 
 if __name__ == '__main__':
