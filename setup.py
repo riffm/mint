b41f835a71b59e53a3b0407610528b4fc0954361
Tim <riffm2005@gmail.com>
2010-03-15 15:47:40 +0300

    mint-import script added

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='mint',
     version='0.1',
     description='Simple indetion based template engine',
+    scripts=['mint/mint-import'],
     author='Tim Perevezentsev',
     author_email='riffm2005@gmail.com',
     packages=['mint']
