babb9e234f8960c531754de857e1ea607b7f7588
Tim Perevezentsev <riffm2005@gmail.com>
2010-07-13 17:34:07 +0400

    ready for first release

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name='mint',
     version='0.1',
     description='Simple indetion based template engine',
-    scripts=['mint/mint-import'],
+    #scripts=['mint/mint-import'],
     author='Tim Perevezentsev',
     author_email='riffm2005@gmail.com',
-    packages=['mint']
+    modules=['mint']
 )
