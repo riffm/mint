37af7ee67154adbf008507bb29e859d14a5738da
Tim Perevezentsev <riffm2005@gmail.com>
2011-01-20 17:18:47 +0300

    template inheritance tests

--- tests.py
+++ tests.py
@@ -138,6 +138,13 @@ class TagsAndText(unittest.TestCase):
                                        '<tag attr="value value1"></tag>')
 
 
+class DummyLoader(object):
+    def __init__(self, templates):
+        self.templates = templates
+    def get_template(self, template_name):
+        return self.templates[template_name]
+
+
 class PythonPart(unittest.TestCase):
 
     def test_expression(self):
@@ -225,6 +232,108 @@ class PythonPart(unittest.TestCase):
         slot = t.slot('count')
         self.assert_(isinstance(slot, types.FunctionType))
         self.assertEqual(slot(1), '1\n')
+
+    def test_inheritance(self):
+        'One level inheritance'
+        loader = DummyLoader({
+            'base.mint':mint.Template('#def slot():\n'
+                                      '    base slot\n'
+                                      '#slot()'),
+        })
+        self.assertEqual(mint.Template('#base: base.mint\n'
+                                       '#def slot():\n'
+                                       '    overrided slot\n', loader=loader).render(),
+                        'overrided slot\n')
+
+    def test_inheritance2(self):
+        'One level inheritance with different slots'
+        loader = DummyLoader({
+            'base.mint':mint.Template('#def slot1():\n'
+                                      '    base slot\n'
+                                      '#slot1()\n'
+                                      '#slot2()'),
+        })
+        self.assertEqual(mint.Template('#base: base.mint\n'
+                                       '#def slot2():\n'
+                                       '    overrided slot\n', loader=loader).render(),
+                        'base slot\noverrided slot\n')
+
+    def test_inheritance3(self):
+        'Two level inheritance'
+        loader = DummyLoader({
+            'base.mint':mint.Template('#def slot():\n'
+                                      '    base slot\n'
+                                      '#slot()'),
+        })
+        loader.templates.update({
+            'base2.mint':mint.Template('#base: base.mint\n'
+                                       '#def slot():\n'
+                                       '    base2 slot\n', loader=loader),
+        })
+        self.assertEqual(mint.Template('#base: base2.mint\n'
+                                       '#def slot():\n'
+                                       '    overrided slot\n', loader=loader).render(),
+                        'overrided slot\n')
+
+    def test_inheritance4(self):
+        'Two level inheritance and slots on differrent levels'
+        loader = DummyLoader({
+            'base.mint':mint.Template('#def slot1():\n'
+                                      '    base slot\n'
+                                      '#slot1()\n'
+                                      '#slot2()\n'
+                                      '#slot3()\n'),
+        })
+        loader.templates.update({
+            'base2.mint':mint.Template('#base: base.mint\n'
+                                       '#def slot2():\n'
+                                       '    base2 slot\n', loader=loader),
+        })
+        self.assertEqual(mint.Template('#base: base2.mint\n'
+                                       '#def slot3():\n'
+                                       '    overrided slot\n', loader=loader).render(),
+                        'base slot\nbase2 slot\noverrided slot\n')
+
+    def test_inheritance5(self):
+        'Two level inheritance and slots on differrent levels 2'
+        loader = DummyLoader({
+            'base.mint':mint.Template('#def slot1():\n'
+                                      '    base slot\n'
+                                      '#slot1()\n'
+                                      '#slot2()\n'
+                                      '#slot3()\n'),
+        })
+        loader.templates.update({
+            'base2.mint':mint.Template('#base: base.mint\n'
+                                       '#def slot2():\n'
+                                       '    base2 slot\n', loader=loader),
+        })
+        self.assertEqual(mint.Template('#base: base2.mint\n'
+                                       '#def slot2():\n'
+                                       '    overrided base2 slot\n'
+                                       '#def slot3():\n'
+                                       '    overrided slot\n', loader=loader).render(),
+                        'base slot\noverrided base2 slot\noverrided slot\n')
+
+    def test_inheritance6(self):
+        'Two level inheritance and __base__'
+        loader = DummyLoader({
+            'base.mint':mint.Template('#def slot():\n'
+                                      '    base slot\n'
+                                      '#slot()'),
+        })
+        loader.templates.update({
+            'base2.mint':mint.Template('#base: base.mint\n'
+                                       '#def slot():\n'
+                                       '    {{ __base__() }}\n'
+                                       '    base2 slot\n', loader=loader),
+        })
+        self.assertEqual(mint.Template('#base: base2.mint\n'
+                                       '#def slot():\n'
+                                       '    {{ __base__() }}\n'
+                                       '    overrided slot\n', loader=loader).render(),
+                        'base slot\n\nbase2 slot\n\noverrided slot\n')
+
 
 
 if __name__ == '__main__':
